07c0b8592bfcd3f6e7c2d92c049471a399d3267d
relxss <relxgo@163.com>
2021-12-06 21:10:35 +0800

    add ethereum connection

---
+++ watcher/chains/ethereum/conn.go
@@ -0,0 +1,57 @@
+package ethereum
+
+import (
+	"context"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/rpc"
+	"math/big"
+)
+
+type Connection struct {
+	endpoint string
+	http     bool
+	conn     *ethclient.Client
+	stop     chan struct{}
+}
+
+func NewConnection(endpoint string, http bool) *Connection {
+	return &Connection{
+		endpoint: endpoint,
+		http:     http,
+	}
+}
+
+// Connect starts the ethereum WS connection
+func (c *Connection) Connect() error {
+	log.Info("Connecting to ethereum chain...", "url", c.endpoint)
+	var rpcClient *rpc.Client
+	var err error
+	// Start http or ws client
+	if c.http {
+		rpcClient, err = rpc.DialHTTP(c.endpoint)
+	} else {
+		rpcClient, err = rpc.DialContext(context.Background(), c.endpoint)
+	}
+	if err != nil {
+		return err
+	}
+	c.conn = ethclient.NewClient(rpcClient)
+	return nil
+}
+
+func (c *Connection) LatestBlock() (*big.Int, error) {
+	header, err := c.conn.HeaderByNumber(context.Background(), nil)
+	if err != nil {
+		return nil, err
+	}
+	return header.Number, nil
+}
+
+// Close terminates the client connection and stops any running routines
+func (c *Connection) Close() {
+	if c.conn != nil {
+		c.conn.Close()
+	}
+	close(c.stop)
+}

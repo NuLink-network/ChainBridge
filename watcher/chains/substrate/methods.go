6ae7ac407a1c2d283c1a344c891ce13d809e63a7
relxss <relxgo@163.com>
2021-12-15 21:56:56 +0800

    add update staker info method

--- watcher/chains/substrate/methods.go
+++ watcher/chains/substrate/methods.go
@@ -5,6 +5,7 @@ type Method string
 const NuProxy = "nuproxy"
 
 var (
-	BaseMethod      Method = ""
-	RegisterWatcher Method = NuProxy + ".register_watcher"
+	BaseMethod       Method = ""
+	RegisterWatcher  Method = NuProxy + ".register_watcher"
+	UpdateStakerInfo Method = NuProxy + ".update_staker_infos_and_mint"
 )

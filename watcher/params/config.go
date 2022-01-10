a7971f7b7262354b716c80f07330dfbaf0b4247b
relxss <relxgo@163.com>
2022-01-10 20:45:45 +0800

    removed useless config

--- watcher/params/config.go
+++ watcher/params/config.go
@@ -1,17 +1,9 @@
 package params
 
 import (
-	"math/big"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-
 	"github.com/centrifuge/go-substrate-rpc-client/v4/signature"
-)
-
-var (
-	WSEndpoint   = ""
-	HTTPEndpoint = ""
 )
 
 var (
@@ -20,15 +12,6 @@ var (
 )
 
 var BlockRetryInterval = time.Second * 5
-
-var (
-	StartBlock         = big.NewInt(10000000)
-	BlockConfirmations = big.NewInt(12)
-)
-
-var (
-	DepositContractAddress = common.HexToAddress("0xBaE99723a3F2374DB1D8287A8679f38aa9c0674d")
-)
 
 var (
 	Watcher = &signature.KeyringPair{
